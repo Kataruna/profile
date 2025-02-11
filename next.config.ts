b3bad3a39b1735eb635a663692b70fdeb9d9d2ee
Kataruna <tuaa1bb2@gmail.com>
2025-02-11 21:43:19 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
+  basePath: '/profile',
 };
 
 export default nextConfig;
